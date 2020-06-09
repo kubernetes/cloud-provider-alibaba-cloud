2c9205a91b23ae090a47a62f2b01ab6428b2f8a1
gujing <925973396@qq.com>
2020-06-09 16:46:34 +0800

    update metric name

--- cloud-controller-manager/utils/metric/route.go
+++ cloud-controller-manager/utils/metric/route.go
@@ -2,16 +2,16 @@ package metric
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
-	"time"
 )
 
 var (
 	// RouteLatency reconcile route latency
-	RouteLatency = prometheus.NewSummaryVec(
-		prometheus.SummaryOpts{
-			Name:   "CCMRouteLatency",
-			Help:   "ccm reconcile route latency in ms",
-			MaxAge: time.Duration(30) * time.Minute,
+	RouteLatency = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "ccm_route_latencies_duration_milliseconds",
+			Help: "CCM route reconcile latency distribution in milliseconds for each verb.",
+			Buckets: []float64{100, 200, 300, 400, 500, 600, 700, 800, 900, 1000,
+				1500, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000},
 		},
 		[]string{"verb"},
 	)
