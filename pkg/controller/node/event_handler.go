db3586772506a74c7ab0939abb201cd952d2bc8e
gujing <zibai.gj@alibaba-inc.com>
2023-09-15 17:45:39 +0800

    refactor node controller

---
+++ pkg/controller/node/event_handler.go
@@ -0,0 +1,48 @@
+package node
+
+import (
+	"context"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/workqueue"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+type enqueueRequestForNodeEvent struct{}
+
+var _ handler.EventHandler = (*enqueueRequestForNodeEvent)(nil)
+
+// NewEnqueueRequestForNodeEvent, event handler for node event
+func NewEnqueueRequestForNodeEvent() *enqueueRequestForNodeEvent {
+	return &enqueueRequestForNodeEvent{}
+}
+
+func (h *enqueueRequestForNodeEvent) Create(_ context.Context, e event.CreateEvent, queue workqueue.RateLimitingInterface) {
+	node, ok := e.Object.(*v1.Node)
+	if ok && needAdd(node) {
+		queue.Add(reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Name: node.Name,
+			},
+		})
+	}
+}
+
+func (h *enqueueRequestForNodeEvent) Update(_ context.Context, e event.UpdateEvent, queue workqueue.RateLimitingInterface) {
+
+}
+
+func (h *enqueueRequestForNodeEvent) Delete(_ context.Context, e event.DeleteEvent, queue workqueue.RateLimitingInterface) {
+
+}
+
+func (h *enqueueRequestForNodeEvent) Generic(_ context.Context, e event.GenericEvent, queue workqueue.RateLimitingInterface) {
+
+}
+
+func needAdd(node *v1.Node) bool {
+	cloudTaint := findCloudTaint(node.Spec.Taints)
+	return cloudTaint != nil
+}
