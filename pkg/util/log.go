3a31ae04837207881e2bac901e2d52b4e9c86621
珩轩 <hengxuan.sh@alibaba-inc.com>
2024-01-23 16:08:00 +0800

    remove elb loger

--- pkg/util/log.go
+++ pkg/util/log.go
@@ -8,11 +8,9 @@ import (
 var (
 	ServiceLog logr.Logger
 	NLBLog     logr.Logger
-	ELBLog     logr.Logger
 )
 
 func init() {
 	ServiceLog = klogr.New().WithName("service-controller")
 	NLBLog = klogr.New().WithName("nlb-controller")
-	ELBLog = klogr.New().WithName("elb-controller")
 }
